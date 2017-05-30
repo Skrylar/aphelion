0c0811002989ec3a86a62ee784926b9cb853f59f
Joshua Cearley <joshua.cearley@gmail.com>
2017-05-30 10:57:27 -0500

    Ported mean squared errors to nim.

---
+++ criterion/meansquarederrors.nim
@@ -0,0 +1,27 @@
+
+import ../tensor
+
+proc mse*(tmp, results, goals: Tensor): float32 =
+   ## calculates mean squared errors for the loss function; ex. for showing
+   ## total loss
+   assert tmp != nil
+   assert results != nil
+   assert goals != nil
+
+   set(tmp, results)
+   sub(tmp, goals)
+   mul(tmp, tmp)
+   mul(tmp, 0.5)
+
+   result = hsum(tmp)
+
+proc mse_derivs*(dest, results, goals: Tensor) =
+   ## calculates mean squared errors for the output layer. this is the
+   ## derivative of the loss function for each output neuron.
+   assert dest != nil
+   assert results != nil
+   assert goals != nil
+
+   set(dest, results)
+   sub(dest, goals)
+
