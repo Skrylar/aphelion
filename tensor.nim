46105f54742687323ed7439d9227d1e8fd01d85f
Joshua Cearley <joshua.cearley@gmail.com>
2017-06-02 19:41:31 -0500

    Made sure weight sum function works properly.

--- tensor.nim
+++ tensor.nim
@@ -174,27 +174,50 @@ proc hsum*(self: Tensor): float32 =
    for i in 0..self.data.high:
       result = result + self.data[i]
 
-proc weight_sum*(self, dest: Tensor, weights, values: int) =
+proc len*(self: Tensor): int =
+  return self.data.len
+
+proc weight_sum*(dest, weights: Tensor, weight_count, value_count: int) =
    ## takes a tensor of weights and sums those in to a smaller operand; so
    ## if 'self' is 10 weights and 5 for each neuron, the operand will have
    ## a length of two, and be filled with the sums of those weights. this
    ## lets us calculate the amount of "fault" a given neuron is at.
 
-   assert(self != nil)
    assert(dest != nil)
-   assert((values %% weights) == 0)
+   assert(weights != nil)
+   assert((weight_count %% value_count) == 0)
 
-   var stripe = weights /% values
+   var stripe_length = weight_count /% valuecount
+   var stripes = weight_count /% stripe_length
    var at = 0
 
-   for i in 0..(values - 1):
-      for j in 0..(stripe-1):
-         dest.data[i] = dest.data[i] + self.data[at]
+   for s in 0..(stripes - 1):
+      for i in 0..(stripe_length - 1):
+         dest.data[s] = dest.data[s] + weights.data[at]
          inc(at)
 
-proc weight_sum*(self, dest: Tensor) =
-   weight_sum(self, dest, self.len, dest.len)
+proc weight_sum*(dest, weights: Tensor) =
+   weight_sum(dest, weights, dest.len, weights.len)
 
-proc len*(self: Tensor): int =
-  return self.data.len
+when isMainModule:
+   import unittest
+
+   suite "Backpropagation primitives":
+      test "Calculating sums of weights going backward":
+         # This operation is done during backpropagation; you take the
+         # weights of neurons in a future layer, work out the weights of
+         # a previous neuron, and distribute the error backwards
+         # proportionately.
+         var a = make_tensor(4)
+         var c = make_tensor(2)
+
+         a[0] = 0.5
+         a[1] = 0.25
+
+         a[2] = 0.25
+         a[3] = 0.1
+
+         weight_sum(c, a, 4, 2)
+         check(abs(c[0] - 0.75) < 0.1)
+         check(abs(c[1] - 0.35) < 0.1)
 
