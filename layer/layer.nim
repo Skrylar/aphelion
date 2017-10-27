e6e274c76ea703f6a939e10d0d99f74624795a46
Joshua Cearley <joshua.cearley@gmail.com>
2017-10-27 11:16:47 -0500

    Collapsed public/private weight split from linear layers.

--- layer/layer.nim
+++ layer/layer.nim
@@ -3,34 +3,22 @@ import ../tensor
 import ../random/random
 
 type
+  ScratchSet* = array[0..4, Tensor]
+
   Layer* = ref object {.inheritable.}
       values*, weights*: Tensor
-      input_count*, value_count*, private_weight_count*: int
+      input_count*, internal_weights*: int
 
-      # scratch space for calculations
-      scratch*: array[0..2, Tensor]
-
-method open*(self: Layer) {.base.} =
+method forward*(self: Layer; inputs: Tensor; scratch: ScratchSet) {.base.} =
    discard
 
-method close*(self: Layer) {.base.} =
+method gradient*(self: Layer; inputs, deltas, total: Tensor; scratch: ScratchSet) {.base.} =
    discard
 
-method forward*(self: Layer, inputs: Tensor) {.base.} =
-   discard
-
-method gradient*(self: Layer, inputs, deltas, total: Tensor) {.base.} =
-   discard
-
-method private_gradient*(self: Layer, inputs, deltas, total: Tensor) {.base.} =
-   discard
- 
-method propagate*(self: Layer, updates: Tensor) {.base.} =
-   discard
-
-method private_propagate*(self: Layer, updates: Tensor) {.base.} =
-   discard
+method propagate*(self: Layer; updates: Tensor; scratch: ScratchSet) {.base.} =
+   self.weights.sub(updates)
 
 method randomize_weights*(self: Layer, rng: Random) {.base.} =
-   discard
+   for i in 0..(self.weights.len - 1):
+      self.weights.set_at(i, rng.next_float);
 
--- layer/linear.nim
+++ layer/linear.nim
@@ -5,55 +5,38 @@ import ../tensor
 
 type
    LinearLayer* = ref object of Layer
-      private_weights: Tensor
 
-method open*(self: LinearLayer) =
-   self.values = make_tensor(self.value_count)
-   assert(self.values != nil)
-   self.weights = make_tensor(self.value_count * self.input_count)
-   assert(self.weights != nil)
-   self.private_weights = make_tensor(self.value_count);
-   assert(self.private_weights != nil)
+method forward*(self: LinearLayer; inputs: Tensor; scratch: ScratchSet) =
+  let n = (self.values.len-1)
+  let a = n*self.input_count
 
-method close*(self: LinearLayer) =
-   self.values          = nil
-   self.weights         = nil
-   self.private_weights = nil
+  # clear all values
+  self.values.set(0)
+  # run weight*value for each neuron
+  inputs.spread(self.weights, self.values, a)
+  # add linear biases
+  self.values.add(self.weights, 0, a, n)
 
-method forward*(self: LinearLayer, inputs: Tensor) =
-   self.values.set(0)
-   inputs.spread(self.weights, self.values)
-   # NB: Not sure we actually need to do this
-   #self.values.divide(float32(inputs.len))
-   self.values.add(self.private_weights)
+method gradient*(self: LinearLayer; inputs, deltas, total: Tensor; scratch: ScratchSet) =
+  let n = (self.values.len-1)
+  let a = n*self.input_count
 
-method gradient*(self: LinearLayer, inputs, deltas, total: Tensor) =
-   # run inputs * weights to find the amount of error 
-   self.scratch[0].set(0)
-   deltas.spread(self.weights, self.scratch[0], self.values.len-1)
-   total.add(self.scratch[0])
-
-method private_gradient*(self: LinearLayer, inputs, deltas, total: Tensor) =
-   # update linear bias weight by the neuron's delta directly
-   total.add(deltas)
-
-method propagate*(self: LinearLayer, updates: Tensor) =
-   self.weights.sub(updates)
-
-method private_propagate*(self: LinearLayer, updates: Tensor) =
-   self.private_weights.sub(updates)
-
-method randomize_weights*(self: LinearLayer, rng: Random) =
-   for i in 0..(self.weights.len - 1):
-      self.weights.set_at(i, rng.next_float);
-
-   for i in 0..(self.private_weights.len - 1):
-      self.private_weights.set_at(i, rng.next_float)
+  # clear the tensor
+  scratch[0].set(0)
+  # weights * deltas to determine error contribution
+  deltas.spread(self.weights, scratch[0], self.values.len-1)
+  # add contributions
+  total.add(scratch[0], 0, 0, a)
+  # also push deltas to update our biases
+  total.add(scratch[0], a, 0, n)
 
 proc make_linear_layer*(inputs, outputs: int): LinearLayer =
    result = LinearLayer()
+   assert result != nil
+   result.internal_weights = 1
    result.input_count = inputs
-   result.value_count = outputs
-   result.private_weight_count = outputs
-   result.open
+   result.values = make_tensor(outputs)
+   assert result.values != nil
+   result.weights = make_tensor((outputs * result.internal_weights) + (outputs * result.input_count))
+   assert result.weights != nil
 
